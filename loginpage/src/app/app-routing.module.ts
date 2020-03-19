3e527f09aa6677a380f5fe7b228759e37c9ca55c
Kundan Kumar <kumarkundan380@gmail.com>
2020-03-19 15:01:34 +0530

    first commit

---
+++ loginpage/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'registration', component:RegistrationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
