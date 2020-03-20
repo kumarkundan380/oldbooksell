be9be1aa58820416b5733d31304c4dc542f32f8c
Kundan Kumar <kumarkundan380@gmail.com>
2020-03-20 15:34:47 +0530

    master file ui changed

---
+++ Angular2/loginpage/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  // showModel($event)
+  // {
+  //   // $('#signup_model').hide();
+  //   console.log($event)
+  // }
+}
