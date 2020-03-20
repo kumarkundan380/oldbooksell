4e2e9b9b12e4d0868c0a7ed0a86a4bb198f8b244
Kundan Kumar <kumarkundan380@gmail.com>
2020-03-20 18:50:29 +0530

    forgot password added

--- Angular2/loginpage/src/app/app-routing.module.ts
+++ Angular2/loginpage/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
-  {path:'registration', component:RegistrationComponent}
+  
 ];
 
 @NgModule({
--- Angular2/loginpage/src/app/app.module.ts
+++ Angular2/loginpage/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegistrationComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
